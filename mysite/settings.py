7aa61722eceefec977f69ca0de0da8b8b46cd2d5
suryanshm000 <suryanshm000@gmail.com>
2021-06-20 15:35:12 +0530

    random commit 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=@mgxkqw74!kb6e%jevk64!2=j%a%g_0m%*ky^n5#(ei*oe4cw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mytechno-blog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
